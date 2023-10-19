5c63ca1d75a6b9ea213136a780b3cbe4ad9bf76d
Quan Tran <hqtran@linagora.com>
2023-10-19 16:31:48 +0700

    [FIX] Run the LSC clean phase to support delete operation
    - https://www.lsc-project.org/documentation/latest/basics.html#clean-phase
    - https://www.lsc-project.org/documentation/latest/run.html?highlight=clean#clean-launch

--- src/test/java/org/lsc/plugins/connectors/james/integration/UserSynchronizationIntegrationTest.java
+++ src/test/java/org/lsc/plugins/connectors/james/integration/UserSynchronizationIntegrationTest.java
@@ -3,10 +3,8 @@ package org.lsc.plugins.connectors.james.integration;
 import static io.restassured.RestAssured.given;
 import static io.restassured.config.EncoderConfig.encoderConfig;
 import static io.restassured.config.RestAssuredConfig.newConfig;
-import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
 
 import java.io.FileReader;
 import java.net.URL;
@@ -27,11 +25,10 @@ import org.bouncycastle.openssl.PEMReader;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
-import org.testcontainers.containers.startupcheck.MinimumDurationRunningStartupCheckStrategy;
 import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
+import org.testcontainers.images.builder.ImageFromDockerfile;
 import org.testcontainers.utility.MountableFile;
 
 import com.auth0.jwt.JWT;
@@ -41,11 +38,12 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
-class AddressMappingSynchronizationIntegrationTest {
+class UserSynchronizationIntegrationTest {
     private static final String DOMAIN = "james.org";
     private static final String BOB = "bob@james.org";
     private static final String ALICE = "alice@james.org";
-    private static final String NOT_EXISTING_IN_LDAP_USER = "onlyExistingInJames@james.org";
+    private static final String MISSING_FIRSTNAME_USER = "missingfirstname@james.org";
+    private static final String ONLY_EXISTING_IN_JAMES_USER = "onlyexistinginjames@james.org";
     private static final URL PRIVATE_KEY = ClassLoader.getSystemResource("conf/jwt_privatekey");
     private static final URL PUBLIC_KEY = ClassLoader.getSystemResource("conf/jwt_publickey");
     private static final int JAMES_WEBADMIN_PORT = 8000;
@@ -103,22 +101,20 @@ class AddressMappingSynchronizationIntegrationTest {
     }
 
     private GenericContainer<?> createLdapContainer(Network network) {
-        return new GenericContainer<>("osixia/openldap:1.5.0")
-            .withClasspathResourceMapping("prepopulated-ldap/populate.ldif",
-                "/container/service/slapd/assets/config/bootstrap/ldif/data.ldif", BindMode.READ_ONLY)
-            .withClasspathResourceMapping("prepopulated-ldap/otherMailboxForAddressMapping.ldif",
-                "/container/service/slapd/assets/config/bootstrap/ldif/otherMailboxForAddressMapping.ldif", BindMode.READ_ONLY)
+        return new GenericContainer<>(
+            new ImageFromDockerfile()
+                .withFileFromClasspath("populate.ldif", "prepopulated-ldap/populate.ldif")
+                .withFileFromClasspath("Dockerfile", "prepopulated-ldap/Dockerfile"))
             .withNetworkAliases("ldap")
             .withNetwork(network)
-            .withEnv("LDAP_DOMAIN", "james.org")
-            .withEnv("LDAP_ADMIN_PASSWORD", "mysecretpassword")
-            .withEnv("LDAP_CONFIG_PASSWORD", "mysecretpassword")
+            .withEnv("SLAPD_DOMAIN", "james.org")
+            .withEnv("SLAPD_PASSWORD", "mysecretpassword")
+            .withEnv("SLAPD_CONFIG_PASSWORD", "mysecretpassword")
             .withCreateContainerCmdModifier(createContainerCmd -> createContainerCmd.withName("team-mail-openldap-testing" + UUID.randomUUID()))
             .withExposedPorts(LDAP_PORT)
-            .withCommand("--copy-service", "--loglevel", "debug")
-            .waitingFor(new LogMessageWaitStrategy().withRegEx(".*slapd starting\\n").withTimes(1)
-                .withStartupTimeout(Duration.ofMinutes(1)))
-            .withStartupCheckStrategy(new MinimumDurationRunningStartupCheckStrategy(Duration.ofSeconds(10)));
+            .waitingFor(new LogMessageWaitStrategy().withRegEx(".*slapd starting*\\n")
+            .withTimes(1)
+            .withStartupTimeout(Duration.ofMinutes(3)));
     }
 
     private GenericContainer<?> createLscContainer(Network network) {
@@ -127,7 +123,7 @@ class AddressMappingSynchronizationIntegrationTest {
             .withNetwork(network)
             .withCommand("./lsc JAVA_OPTS=\"-DLSC.PLUGINS.PACKAGEPATH=org.lsc.plugins.connectors.james.generated\" --config /opt/lsc/conf/ --synchronize all --clean all --threads 1")
             .withCopyFileToContainer(MountableFile.forClasspathResource("lsc-conf/logback.xml"), "/opt/lsc/conf/")
-            .withCopyFileToContainer(MountableFile.forClasspathResource("lsc-conf/address-mapping/lsc.xml"), "/opt/lsc/conf/")
+            .withCopyFileToContainer(MountableFile.forClasspathResource("lsc-conf/user/lsc.xml"), "/opt/lsc/conf/")
             .withCreateContainerCmdModifier(createContainerCmd -> createContainerCmd.withName("tmail-lsc-testing" + UUID.randomUUID()));
     }
 
@@ -171,132 +167,47 @@ class AddressMappingSynchronizationIntegrationTest {
         given(requestSpecification)
             .body("{\"password\":\"" + RandomStringUtils.randomAlphanumeric(24) + "\"}")
             .basePath("/users")
-        .put("/{user}", user)
+            .put("/{user}", user)
             .then()
             .statusCode(HttpStatus.SC_NO_CONTENT);
     }
 
-    private void removeUser(String user) {
-        given(requestSpecification)
-            .basePath("/users")
-            .delete("/{user}", user)
-        .then()
-            .statusCode(HttpStatus.SC_NO_CONTENT);
-    }
-
-    private void createAddressMapping(String user, String addressMapping) {
-        given(requestSpecification)
-            .post("/mappings/address/{user}/targets/{addressMapping}", user, addressMapping)
-        .then()
-            .statusCode(HttpStatus.SC_NO_CONTENT);
-    }
-
     @Test
-    void lscJobShouldCreateAddressMappingsInJamesForAllUsers() {
-        // GIVEN BOB and ALICE entries in LDAP. BOB has 2 address mappings and ALICE has 1 address mapping in LDAP and no in James.
-        createUser(BOB);
-        createUser(ALICE);
-
-        // RUN the LSC address mapping job
+    void userSyncJobShouldAddUsersFromLDAPToTMail() {
+        // Run the LSC user sync job (LDAP entries: bob@james.org, alice@james.org, missingFirstname@james.org)
         runLscContainer();
 
-        // THEN BOB and ALICE should have address mappings created in James
         await.untilAsserted(() -> {
-            assertThatCode(() -> {
-                given(requestSpecification)
-                    .get(String.format("/mappings/user/%s", BOB))
-                .then()
-                    .statusCode(HttpStatus.SC_OK)
-                    .body("mapping", hasSize(2))
-                    .body("[0].mapping", equalTo("addressMapping1@gmail.com"))
-                    .body("[1].mapping", equalTo("addressMapping2@gmail.com"));
+            String response = given(requestSpecification)
+                .get("/users")
+            .then()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .jsonPath()
+                .getString("");
 
-                given(requestSpecification)
-                    .get(String.format("/mappings/user/%s", ALICE))
-                .then()
-                    .statusCode(HttpStatus.SC_OK)
-                    .body("mapping", hasSize(1))
-                    .body("[0].mapping", equalTo("addressMapping1@gmail.com"));
-            }).doesNotThrowAnyException();
+            assertThat(response).contains(BOB, ALICE, MISSING_FIRSTNAME_USER);
         });
     }
 
     @Test
-    void lscJobShouldNotCreateAddressMappingsWhenUserDoesNotExistYetInJames() {
-        // GIVEN BOB entry in LDAP. BOB has 2 address mappings in LDAP, but BOB does not exist yet in James.
-        removeUser(BOB);
+    void userSyncJobShouldRemoveDanglingUserInTMail() {
+        createUser(ONLY_EXISTING_IN_JAMES_USER);
 
-        // RUN the LSC address mapping job
+        // Run the LSC user sync job (onlyexistinginjames@james.org is not in the LDAP entries)
         runLscContainer();
 
-        // THEN BOB address mappings should not be created yet (wait for BOB user to be created first).
-        await.untilAsserted(() -> assertThatCode(() ->
-                given(requestSpecification)
-                    .get(String.format("/mappings/user/%s", BOB))
-                .then()
-                    .statusCode(HttpStatus.SC_OK)
-                    .body("mapping", hasSize(0)))
-                .doesNotThrowAnyException());
-    }
-
-    @Test
-    void lscJobShouldCreateMissingAddressMappingInJames() {
-        // GIVEN BOB entry in LDAP. BOB has 2 address mappings in LDAP, but only 1 address mapping in James.
-        createUser(BOB);
-        createAddressMapping(BOB, "addressMapping1@gmail.com");
-
-        // RUN the LSC address mapping job
-        runLscContainer();
-
-        // THEN the missing address mapping in LDAP should be created in James.
-        await.untilAsserted(() -> assertThatCode(() ->
-            given(requestSpecification)
-                .get(String.format("/mappings/user/%s", BOB))
+        // THEN the user ONLY_EXISTING_IN_JAMES_USER should be deleted out of TMail
+        await.untilAsserted(() -> {
+            String response = given(requestSpecification)
+                .get("/users")
             .then()
                 .statusCode(HttpStatus.SC_OK)
-                .body("mapping", hasSize(2))
-                .body("[0].mapping", equalTo("addressMapping1@gmail.com"))
-                .body("[1].mapping", equalTo("addressMapping2@gmail.com")))
-            .doesNotThrowAnyException());
-    }
+                .extract()
+                .jsonPath()
+                .getString("");
 
-    @Test
-    void lscJobShouldRemoveDanglingAddressMappingInJames() {
-        // GIVEN ALICE entry in LDAP. ALICE has 1 address mapping in LDAP, but 2 address mapping in James.
-        createUser(ALICE);
-        createAddressMapping(ALICE, "addressMapping1@gmail.com");
-        createAddressMapping(ALICE, "addressMapping2@gmail.com");
-
-        // RUN the LSC address mapping job
-        runLscContainer();
-
-        // THEN the dangling address mapping in James should be removed.
-        await.untilAsserted(() -> assertThatCode(() ->
-                given(requestSpecification)
-                    .get(String.format("/mappings/user/%s", ALICE))
-                .then()
-                    .statusCode(HttpStatus.SC_OK)
-                    .body("mapping", hasSize(1))
-                    .body("[0].mapping", equalTo("addressMapping1@gmail.com")))
-                .doesNotThrowAnyException());
-    }
-
-    @Test
-    void lscJobShouldRemoveAllAddressMappingsOfDanglingUserInJames() {
-        // Provision some address mappings in James for the user
-        createUser(NOT_EXISTING_IN_LDAP_USER);
-        createAddressMapping(NOT_EXISTING_IN_LDAP_USER, "addressMapping1@gmail.com");
-        createAddressMapping(NOT_EXISTING_IN_LDAP_USER, "addressMapping2@gmail.com");
-
-        // WHEN the user entry is removed in LDAP, and run the LSC address mapping job
-        runLscContainer();
-
-        // THEN all address mappings of the user should be cleared.
-        await.untilAsserted(() ->
-            given(requestSpecification)
-                .get(String.format("/mappings/user/%s", NOT_EXISTING_IN_LDAP_USER))
-            .then()
-                .statusCode(HttpStatus.SC_OK)
-                .body("mapping", hasSize(0)));
+            assertThat(response).doesNotContain(ONLY_EXISTING_IN_JAMES_USER);
+        });
     }
 }
